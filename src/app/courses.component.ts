3f9ecd087e70e841ea75d053f6c94b064485e2e6
riona-philips <rionapristin.fernandes@philips.com>
2024-02-06 23:36:41 +0530

    first commit

---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'courses',
+    templateUrl:'./courses.component.html',
+    styleUrl: './courses.component.css',
+    standalone: true
+})
+export class CoursesComponent{
+
+}
